c5b5c880ece03613575766ef8b87b86240b72974
Tianrun <wshhrm@163.com>
2019-02-21 19:18:38 +1100

    1

--- Cassini/CassiniViewController.swift
+++ Cassini/CassiniViewController.swift
@@ -59,12 +59,4 @@ class CassiniViewController: UIViewController,UISplitViewControllerDelegate {
 }
 
 
-extension UIViewController{
-    var contents:UIViewController{
-        if let navcon = self as?UINavigationController{
-            return navcon.visibleViewController ?? self
-        }else{
-            return self
-        }
-    }
-}
+
